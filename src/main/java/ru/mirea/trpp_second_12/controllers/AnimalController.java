3bb0cb6adbfd96e2a6e2f374141ded5e66a08121
Kirill00049 <97191260+Kirill00049@users.noreply.github.com>
2023-03-26 17:46:47 +0300

    Add files via upload
    First commit

--- src/main/java/ru/mirea/trpp_second_12/controllers/AnimalController.java
+++ src/main/java/ru/mirea/trpp_second_12/controllers/AnimalController.java
@@ -4,6 +4,7 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import ru.mirea.trpp_second_12.entity.Animal;
 
 import java.io.InputStreamReader;
 import java.util.List;
--- src/main/java/ru/mirea/trpp_second_12/entity/Animal.java
+++ src/main/java/ru/mirea/trpp_second_12/entity/Animal.java
@@ -5,8 +5,7 @@ import com.opencsv.bean.CsvBindByName;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
-import java.math.BigInteger;
+import java.math.BigDecimal;
 
 /** Сущность животного. */
 @Getter
